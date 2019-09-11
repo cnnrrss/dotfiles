cfe8d9f8f23bd0293b882a844abd3a090bbadf5e
Connor Vanderhook <14183191+cnnrrss@users.noreply.github.com>
2019-09-11 10:03:15 +1000

    fix: don't be smart

--- brew_cask.sh
+++ brew_cask.sh
@@ -10,28 +10,24 @@ brew upgrade
 
 brew install brew-cask
 
-apps=(
-    alfred
-    amphetamine
-    google-chrome
-    dash
-    docker
-    dropbox
-    firefox
-    iterm2
-    moom
-    postman
-    slack
-    sourcetree
-    spotify
-    sublime-text
-    visual-studio-code
-    ideaIU-2017.3.7
-)
-
 echo "Install Apps"
 
-brew cask install --appdir="/Applications" ${apps[@]}
+brew cask install --appdir="~/Applications" alfred
+brew cask install --appdir="~/Applications" amphetamine
+brew cask install --appdir="~/Applications" google-chrome
+brew cask install --appdir="~/Applications" dash
+brew cask install --appdir="~/Applications" docker
+brew cask install --appdir="~/Applications" dropbox
+brew cask install --appdir="~/Applications" firefox
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" moom
+brew cask install --appdir="~/Applications" postman
+brew cask install --appdir="~/Applications" slack
+brew cask install --appdir="~/Applications" sourcetree
+brew cask install --appdir="~/Applications" spotify
+brew cask install --appdir="~/Applications" sublime-text
+brew cask install --appdir="~/Applications" visual-studio-code
+# brew cask install --appdir="~/Applications" ideaIU-2017.3.7
 
 brew cleanup
 
